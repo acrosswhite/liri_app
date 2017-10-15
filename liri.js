61e1312f893f76a17c7b6e137eb82293d0bd2d6d
Ashley Crosswhite <ar.crosswhite@gmail.com>
2017-10-15 11:20:20 -0700

    done except defaults

--- liri.js
+++ liri.js
@@ -1,29 +1,39 @@
 var keys = require("./keys.js");
 var spotify = require('node-spotify-api');
-
-//commands my-tweets, spotify-this-song, movie-this, do-what-it-says
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotifyKeys);
-var song = encodeURIComponent(process.argv[3]);
+var song = process.argv[3];
 var Twitter = require('twitter');
 var client = new Twitter(keys.twitterKeys);
 var params = {screen_name: 'ash_crosswhite'};
 var movieName = encodeURIComponent(process.argv[3]);
 var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
 var request = require("request");
+var fs = require("fs");
+var textFile = process.argv[3];
+
+//commands my-tweets, spotify-this-song, movie-this, do-what-it-says
+
 
 if (process.argv[2] === "spotify-this-song") {
 	spotify
 	  .search({ 
 	  	type: 'track', 
 	  	query: song 
+	  }, (function(err, response) {
+	  	if(err){
+	  		console.log("error");
+	  	}
+	    else {
+	    	var songInfo = response.tracks.items[0];
+	    	console.log(songInfo.name);
+	    	console.log(songInfo.artists[0].name);
+	    	console.log(songInfo.album.name);
+	    	console.log(songInfo.preview_url);
+	    	
+	    }
 	  })
-	  .then(function(response) {
-	    console.log(response);
-	  })
-	  .catch(function(err) {
-	    console.log(err);
-	 });
+	  )
  }
  else if (process.argv[2] === "my-tweets"){
 
@@ -63,7 +73,20 @@ else if (process.argv[2] === "movie-this"){
 
 else if (process.argv[2] === "do-what-it-says"){
 	//fs node package to run text in random.txt
+	fs.readFile("random.txt", "utf8", function(error, data) {
+
+	  if (error) {
+	    return console.log(error);
+	  }
+
+	  console.log(data);
+
+	  var dataArr = data.split(",");
+
+	  console.log(dataArr);
+
+	});
 }
-else{
+else {
 	console.log("not a valid entry please select: my-tweets, movie-this, spotify-this-song, or do-what-it-says")
 }
